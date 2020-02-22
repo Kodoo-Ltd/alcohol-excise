21ba1647e207a6ba6d7e9e81ac4df12a5a6f4c6a
James Carr-Saunders <jamescs@btinternet.com>
2020-02-22 19:41:51 +0000

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name':'Excise Tracker',
+    'description':'Track excise on movements of goods subject to excise',
+    'author':'James Carr-Saunders',
+    'depends':['stock'],
+    'application':True,
+    'installable': True,
+}
