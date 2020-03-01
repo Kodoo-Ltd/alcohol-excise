ca75ad40a2f33a89c3906e63d3955f00da105953
James Carr-Saunders <jamescs@btinternet.com>
2020-03-01 16:16:41 +0000

    20200301

--- __manifest__.py
+++ __manifest__.py
@@ -1,14 +1,15 @@
 {
-    'name':'Excise Tracker',
-    'description':'Track excise on movements of goods subject to excise',
+    'name':'Excise - Alcohol',
+    'description':'Track excise on movements of alcoholic beverages subject to excise',
     'author':'James Carr-Saunders',
     'depends':['stock'],
     'application':True,
     'installable': True,
     'data': [
-        'views/excise_category_tree.xml',
+        'views/excise_category_views.xml',
         'views/excise_menu.xml',
         'security/ir.model.access.csv',
-#        'views/barcodes_templates.xml',
+        'data/excise_category_data.xml',
+        #'data/excise_category_rate_data.xml',
     ],
 }
--- models/excisesetup.py
+++ models/excisesetup.py
@@ -12,4 +12,26 @@ class excise_category(models.Model):
         ('hectoprod','Rate per hectolitre of product'),   
         ('litrealco','Rate per litre of pure alcohol')
     ])
+    #rate_ids = fields.One2many('excise.category.rate', 'category_id', string='Rates')
+    rate = fields.Float(compute='_compute_current_rate', string='Current Rate', digits=0,
+                    help='The rate of the currency to the currency of rate 1.')
+
     add_cat = fields.Many2one('excise_category','Additional Category')
+
+    #@api.depends(self) //date?
+    def _compute_current_rate(self):
+        #date = self._context.get('date') or fields.Date.today()
+        #currency_rates = self._get_rates(company, date)
+        for cat in self:
+        #    cat.rate = currency_rates.get(currency.id) or 0
+            cat.rate = 1
+
+
+class excise_category_rate(models.Model):
+    _name = 'excise.category.rate'
+    _description = 'Excise Rate'
+
+    name = fields.Date(string='Start Date', required=True, index=True,
+                           default=lambda self: fields.Date.today())
+    category_id = fields.Many2one('excise.category', string='Category', readonly=True)
+    rate = fields.Float('Rate')
